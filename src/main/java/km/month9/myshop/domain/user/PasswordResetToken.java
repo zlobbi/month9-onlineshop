c6f2a04d6c1efd94545ea6fa462bf648c01834d3
kadyrbek90 <kadyrbek.mavlyanov@gmail.com>
2020-05-19 13:10:46 +0600

    some changes

---
+++ src/main/java/km/month9/myshop/domain/user/PasswordResetToken.java
@@ -0,0 +1,43 @@
+package km.month9.myshop.domain.user;
+
+import km.month9.myshop.domain.cart.Cart;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+@Entity
+@Builder
+@Table(name = "User")
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank
+    @Size(min = 4, message = "Login must contain min 4 symbols")
+    @Column(length = 128)
+    private String login;
+    @NotBlank
+    @Column(length = 128)
+    private @Email String email;
+    @NotBlank
+    @Column(length = 128)
+    @Size(min = 6, message = "Password must contain min 6 symbols")
+    private String password;
+    @Column
+    @Builder.Default
+    private boolean enabled = true;
+    @NotBlank
+    @Size(min = 1, max = 128)
+    @Column(length = 128)
+    @Builder.Default
+    private String role = "USER";
+//    @OneToOne(cascade = CascadeType.ALL)
+//    @JoinColumn(name = "cart_id", referencedColumnName = "id")
+//    private Cart cart;
+}
